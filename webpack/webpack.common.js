67d90828c000c655935bd49ae12b9df7ffc7ca55
RajatArora <arora@signal7.de>
2020-01-10 22:18:43 +0100

    feat: displayed hello world

---
+++ webpack/webpack.common.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
+
+};
+
