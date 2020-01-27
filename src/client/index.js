587fdee926bbf616ba9ec22ae61fd02cda77652a
Rajat Arora <arora@signal7.de>
2020-01-27 22:58:46 +0100

    feat: new feat

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import Game from './updownapp.js';
 
 function App () {
-  return <h1>Hello World!!</h1>;
+  return <h1>Hello World!!
+    <Game></Game>
+  </h1>
+
 }
 
 render(<App />, document.getElementById('root'));
